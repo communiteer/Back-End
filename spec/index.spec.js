d13ed678e18f4090ad00a94347a5ea72a6cb4932
wasan salih <wassansalih@hotmail.com>
2017-07-18 12:06:03 +0100

    test the controller function getGroupById

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,14 +1,14 @@
 process.env.NODE_ENV = 'test';
 const expect = require('chai').expect;
 const path = require('path');
-const {getUserById} = require(path.resolve(__dirname,'..','src','controllers'));
+const {getUserById, getGroupById} = require(path.resolve(__dirname,'..','src','controllers'));
 const server = require('../server');
 
 const request = require('supertest');
 
 describe('TEST ALL THE ROUTES', () => {
 	describe('GET-USER-BY-ID', () => {
-		it('is afunction', () => {
+		it('is a function', () => {
 			expect(getUserById).to.be.a('function');
 		});
 		it('should return the user information using user id', (done) => {
@@ -23,4 +23,24 @@ describe('TEST ALL THE ROUTES', () => {
 			});
 		});
 	});
+
+	describe('GET-GROUP-BY-ID', () => {
+		it('is a function', () => {
+			expect(getGroupById).to.be.a('function');
+		});
+		it('should return the group information using group id', (done) => {
+			request(server)
+			.get('/groups/1')
+			.end((err,res) => {
+				expect(res.status).to.equal(200);
+				expect(res.body).to.be.an('object');
+				expect(res.body.data).to.be.an('array');
+				expect(res.body.data[0].group_id).to.equal(1);
+				expect(res.body.data[0].admin_name).to.equal('Ben');
+				expect(res.body.data[0].group_name).to.equal('A');
+				done();
+			});
+		});
+	});
+
 });
