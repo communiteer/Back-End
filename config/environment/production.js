dfc0d4e958b53abd27e1d2c821dbd6f10e08aead
wasan salih <wassansalih@hotmail.com>
2017-07-24 12:35:54 +0100

    change the data

--- config/environment/production.js
+++ config/environment/production.js
@@ -1,11 +1,11 @@
 module.exports = {
     env: 'production',
     db: {
-        host: 'ec2-23-23-225-12.compute-1.amazonaws.com',
+        host: 'ec2-54-221-220-82.compute-1.amazonaws.com',
         port: 5432,
-        database: 'dbcsmjtpl2ldlu',
-				user: 'pmwkxiofsshybu',
-				password: 'a0fbd2dbe1a69b99856e05efc64c40ead20fa0e2a2fe3552f268c24578cad2b1'
+        database: 'd4lhi6uimjl70i',
+				user: 'pchbffaxvkzdes',
+				password: '625860fa0ddeff4289511bbc59930728dd860052dfb95924ad89b11f52c96703'
     },
     port: 3000
 };
