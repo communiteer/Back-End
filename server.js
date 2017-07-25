bac2546a4f9c111dc161e227c67d8f2f1c4f2c6f
Kamran <kamran.mujtaba@hotmail.com>
2017-07-25 17:29:40 +0100

    added cors

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const config = require('./config');
 const PORT = process.env.PORT || config.port;
 const router = require(path.resolve(__dirname, 'src', 'router', 'routes'));
 const jsonParser = require('body-parser').json;
+const cors = require('cors');
 
 app.use(jsonParser());
+app.use(cors());
 app.use(router);
 
 app.listen(PORT, function() {
